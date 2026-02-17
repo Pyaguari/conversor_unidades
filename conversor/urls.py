65175dcdcf2115d15455b76f17fbc15bf41cf3d2
Pablo Yaguar <warspablo@gmail.com>
2026-02-17 16:06:23 -0500

    testing

--- conversor/urls.py
+++ conversor/urls.py
@@ -6,4 +6,6 @@ app_name = "conversor"
 urlpatterns = [
     path("", views.seleccionar_magnitud, name="seleccionar_magnitud"),
     path("convertir/<int:magnitud_id>/", views.convertir_view, name="convertir"),
+    path('debug/<int:magnitud_id>/', views.debug_magnitud, name='debug'),  # ← Temporal
+   
 ]
